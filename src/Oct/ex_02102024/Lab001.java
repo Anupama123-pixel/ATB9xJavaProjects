88ff496a16a50cf8609a1c736a4a6b1917e5f077
Anupama <anupama.yelmadgi@gmail.com>
2024-10-04 06:59:55 +0530

    Test

---
+++ src/Oct/ex_02102024/Lab001.java
@@ -0,0 +1,10 @@
+package Oct.ex_02102024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        for(int i= 1; i<=10; i++){
+            int TableOf10s = i*10;
+            System.out.println("10*"+i=TableOf10s);
+        }
+}
+}
