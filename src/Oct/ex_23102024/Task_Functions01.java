2bd619fb53e39ff208f16934aa9cfcd076ae8569
Anupama <anupama.yelmadgi@gmail.com>
2024-10-24 11:48:31 +0530

    Functions(Arithmetic operators)

--- src/Oct/ex_23102024/Task_Functions01.java
+++ src/Oct/ex_23102024/Task_Functions01.java
@@ -7,35 +7,46 @@ public class Task_Functions01 {
         System.out.println("Arithmetic Operaters");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first value- ");
+        while(!sc.hasNextInt()){
+            System.out.println("Enetered input is not valid:");
+            sc.next();
+            System.out.println("Enter an integer:");
+        }
         int a = sc.nextInt();
-        System.out.println("Enter the first value- ");
+        System.out.println("Enter the Second value- ");
+        while(!sc.hasNextInt()){
+            System.out.println("Enetered input is not valid:");
+            sc.next();
+            System.out.println("Enter an integer:");
+        }
         int b = sc.nextInt();
-       int sum = Sum(a,b);
+        int sum = Sum(a,b);
         System.out.println("summation->"+ sum);
         int sub = Sub(a,b);
-        System.out.println("summation->"+ sub);
+        System.out.println("substraction->"+ sub);
         int mul = Mul(a,b);
-        System.out.println("summation->"+ mul);
+        System.out.println("multiplication->"+ mul);
         int div = Div(a,b);
-        System.out.println("summation->"+ div);
+        System.out.println("division->"+ div);
+        int mod = Div(a,b);
+        System.out.println("modulus->"+ mod);
+        sc.close();
     }
     static int Sum(int a, int b){
-       int sum = a+b;
-       return sum;
+       return a+b;
     }
     static int Sub(int a, int b) {
-        int sub = a - b;
-         return sub;
+         return a - b;
     }
     static int Mul(int a, int b){
-        int mul = a*b;
-        return mul;
+        return a*b;
     }
     static int Div(int a, int b){
-        int div = a/b;
-        return div ;
-
-
+        return a/b ;
     }
+    static int mod(int a, int b){
+        return a % b ;
+    }
+
 }
 
